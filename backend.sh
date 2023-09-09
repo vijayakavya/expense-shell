b77aafccbe2117e47bc4309eebd82491c83cf847
kavya <vijayakavya1226@gmail.com>
2023-09-09 16:45:42 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -5,6 +5,7 @@ dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
