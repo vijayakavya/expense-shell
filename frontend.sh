cf7ce8fde0fd44cecd65c2c513d97fa349132398
kavya <vijayakavya1226@gmail.com>
2023-09-09 23:57:52 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -3,19 +3,19 @@ source common.sh
 component=frontend
 echo Installing Nginx
 
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_file
 
 echo Placing Expense Config File in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo Removing Old Nginx Content
 
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 cd /usr/share/nginx/html
 
 download_and_extract
 
 echo Starting nginx Service
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,17 @@
 source common.sh
 
 echo Disable MYSQL 8 Version
-dnf module disable mysql -y >>$log_file
+dnf module disable mysql -y &>>$log_file
 
 echo Copy MySQL Repo File
-cp mysql.repo /etc/yum.repos.d/mysql.repo >>$log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 
 echo Install MySQL Server
-dnf install mysql-community-server -y >>$log_file
+dnf install mysql-community-server -y &>>$log_file
 
 echo Start MySQL Service
-systemctl enable mysqld >>$log_file
-systemctl start mysqld >>$log_file
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 
 echo Setup root Password
-mysql_secure_installation --set-root-pass ExpenseApp@1 >>$log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
