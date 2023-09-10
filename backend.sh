e27aef92734f5777f3f1a336cc86149d7b9845fd
kavya <vijayakavya1226@gmail.com>
2023-09-10 18:52:41 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -5,7 +5,7 @@ echo Install NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 stat_check()
 
-echo install NodeJS
+echo Install NodeJS
 dnf install nodejs -y &>>$log_file
 stat_check()
 
