63406032c1f76371fd2f8b62a5c11f67aabc968f
kavya <vijayakavya1226@gmail.com>
2023-09-10 15:48:29 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -7,6 +7,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 fi
 
 echo install NodeJS
@@ -15,6 +16,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 fi
 
 echo Copy Backend Service File
@@ -23,6 +25,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 fi
 
 echo Add Application User
@@ -31,6 +34,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 fi
 
 echo Clean App Content
@@ -39,6 +43,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 fi
 
 mkdir /app
